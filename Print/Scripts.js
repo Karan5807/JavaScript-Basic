345ab1fde690582796359b4151f4d554802d01c0
Karan Singh <90771675+Karan5807@users.noreply.github.com>
2023-02-03 15:41:54 +0530

    Add files via upload

---
+++ Print/Scripts.js
@@ -0,0 +1,11 @@
+let Btn = document.getElementById("Btn");
+console.log(Btn);
+
+function Print(){
+ window.print();
+}
+
+function Code(){
+    let Sc = document.getElementById("Sc");
+    window.open(Sc);
+}
