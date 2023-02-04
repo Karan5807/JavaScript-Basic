fb1b799793ab4efe681da0a0a58f91554e5423b7
Karan Singh <90771675+Karan5807@users.noreply.github.com>
2023-02-04 22:09:45 +0530

    Add files via upload

---
+++ LeapYear/Scripts.js
@@ -0,0 +1,50 @@
+let Btn = document.getElementById("Btn");
+console.log(Btn);
+
+function Leap(){
+
+   /////////  Getting values from the Sides ///////
+ let Year = parseInt(document.getElementById("Year").value);
+ console.log(Year);
+ 
+
+
+ //////////// Code for Calculate Side ////////
+ if(Year%4 ==0 && Year%100 != 0 || Year%400 == 0)
+ {
+  let Answer = document.getElementById("Answer").innerHTML=`
+ <p class="fs-2 mt-2 text-center px-2 border rounded-3 border-3 bg-white"> Leap Year = ${Year}</p>
+ `;
+ }
+
+ else
+ {
+  let Answer = document.getElementById("Answer").innerHTML=`
+ <p class="fs-2 mt-2 text-center px-2 border rounded-3 border-3 bg-white"> Non Leap Year = ${Year}</p>
+ `;
+ }
+
+}
+
+
+ ///////// Code for Calculate Area /////////*
+ /*
+ let A = Math.sqrt(semiPeri*(semiPeri-FSide)*(semiPeri-SSide)*(semiPeri-LSide));
+
+ ////////// Convert the Code into 2 Decimal Places //////////
+ let Area = A.toFixed(2);
+
+ console.log(Area);
+   //////// Printing the Output  ////////
+
+ let Answer = document.getElementById("Answer").innerHTML=`
+ <p class="fs-2 mt-2 text-center px-2 border rounded-3 border-3 bg-white"> The Area of Triangle = ${Area}</p>
+ `;
+}
+
+*/
+
+function Code(){
+    let Sc = document.getElementById("Sc");
+    window.open(Sc);
+}
